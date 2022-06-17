a4345011933d1d88574da745fc0af054036026a5
Diogo San'Anna <diogo.santanna@outlook.com.br>
2022-06-17 15:19:05 -0300

    test: testa o relatório individual do produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,24 @@
-# from inventory_report.inventory.product import Product
+from typing import cast
+from inventory_report.inventory.product import Product
+
+assert_product = (
+    "O produto Biscoito Recheado de Limão"
+    + " fabricado em 17-06-2022"
+    + " por Piraquê"
+    + " com validade até 17-12-2022"
+    + " precisa ser armazenado em local seco e arejado."
+)
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        id=1,
+        nome_do_produto="Biscoito Recheado de Limão",
+        nome_da_empresa="Piraquê",
+        data_de_fabricacao="17-06-2022",
+        data_de_validade="17-12-2022",
+        numero_de_serie="12345678",
+        instrucoes_de_armazenamento="em local seco e arejado",
+    )
+
+    assert str(cast(typ=str, val=product)) == assert_product

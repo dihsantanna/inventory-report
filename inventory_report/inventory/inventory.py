9ca088c4f76c514b2cb4e565fa3fedc21111c42f
Diogo San'Anna <diogo.santanna@outlook.com.br>
2022-06-20 16:50:27 -0300

    feat: implementa classe inventory com funcionalidade que gera os relatórios através de um arquivo XML

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import xml.etree.ElementTree as ET
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -14,9 +15,21 @@ class Inventory:
                 reader = csv.reader(file, delimiter=",", quotechar='"')
                 header, *rows = reader
                 product_list = list(dict(zip(header, row)) for row in rows)
+
         elif path.endswith(".json"):
             with open(path, encoding="utf-8") as file:
                 product_list = json.load(file)
+
+        elif path.endswith(".xml"):
+            tree = ET.parse(path)
+            root = tree.getroot()
+
+            for child in root:
+                product = dict()
+                for attr in child:
+                    product[attr.tag] = attr.text
+
+                product_list.append(product)
 
         if type == "simples":
             return SimpleReport.generate(product_list)
